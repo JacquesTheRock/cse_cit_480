2517dfb71451d934a73fe624d05729a66f09bf8f
PostgreSQL Server <postgres@localhost.localdomain>
2016-09-26 23:38:44 -0400

    Add husk for creating the rw user

---
+++ sql/auth/rw.sql
@@ -0,0 +1,13 @@
+CREATE ROLE bloom_rw WITH PASSWORD '';
+ALTER ROLE bloom_rw WITH LOGIN;
+GRANT CONNECT ON DATABASE bloomdb TO bloom_rw; 
+GRANT ALL ON users TO bloom_rw;
+GRANT ALL ON logins TO bloom_rw;
+GRANT ALL ON role_t TO bloom_rw;
+GRANT ALL ON roles TO bloom_rw;
+GRANT ALL ON project TO bloom_rw;
+GRANT ALL ON crosses TO bloom_rw;
+GRANT ALL ON specimen TO bloom_rw;
+GRANT ALL ON specimen_trait TO bloom_rw;
+GRANT ALL ON trait TO bloom_rw;
+GRANT ALL ON trait_t TO bloom_rw;
