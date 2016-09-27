79b5266556e422ddad4ba12e5abc6a4d4712b9cd
PostgreSQL Server <postgres@localhost.localdomain>
2016-09-27 03:25:02 -0400

    Enforce not null requirement on fields

--- sql/tables/project.sql
+++ sql/tables/project.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS project (
 	id serial,
-	name VARCHAR(255),
-	visibility INTEGER,
+	name VARCHAR(255) NOT NULL,
+	description text,
+	visibility INTEGER NOT NULL,
 	PRIMARY KEY(id)
 );
