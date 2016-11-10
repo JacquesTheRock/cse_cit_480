9379f7b66584b376023b11b38639ae2fff926e5b
Jacques Breaux <jabreaux@oakland.edu>
2016-11-10 16:24:51 +0000

    Add defaults

--- sql/tables/project.sql
+++ sql/tables/project.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS project (
 	id serial,
 	name VARCHAR(255) NOT NULL,
 	description text,
-	location text,
-	species text,
-	pType text,
+	location text DEFAULT '',
+	species text DEFAULT '',
+	pType text DEFAULT '',
 	visibility INTEGER NOT NULL,
 	PRIMARY KEY(id)
 );
