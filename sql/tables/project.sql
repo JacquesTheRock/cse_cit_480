0f459527e72fa68a099f731ce76148a15b11a3d1
Jacques Breaux <jabreaux@oakland.edu>
2016-11-10 16:18:42 +0000

    new project fields

--- sql/tables/project.sql
+++ sql/tables/project.sql
@@ -2,6 +2,9 @@ CREATE TABLE IF NOT EXISTS project (
 	id serial,
 	name VARCHAR(255) NOT NULL,
 	description text,
+	location text,
+	species text,
+	pType text,
 	visibility INTEGER NOT NULL,
 	PRIMARY KEY(id)
 );
