afd430610f72bb49d76c53f317925dca9ad12de3
Jacques Breaux <jabreaux@oakland.edu>
2016-11-21 23:02:38 +0000

    Set location to default value

--- sql/data/01users.sql
+++ sql/data/01users.sql
@@ -1,7 +1,7 @@
-INSERT INTO users(id,email,name,location,hash,salt, ALGORITHM) Values
-('admin','noreply@bloomgenetics.tech','Admin','','password','','PLAIN'),
-('george','noreply@bloomgenetics.tech','Georgino','','georgies password','','PLAIN'),
-('guest','noreply@bloomgenetics.tech','Guest who', '','','','NOLOGIN');
+INSERT INTO users(id,email,name,hash,salt, ALGORITHM) Values
+('admin','noreply@bloomgenetics.tech','Admin','password','','PLAIN'),
+('george','noreply@bloomgenetics.tech','Georgino','georgies password','','PLAIN'),
+('guest','noreply@bloomgenetics.tech','Guest who','','','NOLOGIN');
 
 
 INSERT INTO mail(src, dest, message,prev) VALUES
